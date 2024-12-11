69b160b0093ec464474540410e617e43ae39c478
KricsDavid <david@kricsfalusi.hu>
2024-12-11 09:42:35 +0100

    error

--- users/src/components/UserCard.tsx
+++ users/src/components/UserCard.tsx
@@ -1,5 +1,25 @@
+import { useState } from "react"
 
 const UserCard = () => {
+
+  const [searchUser, setSearchUser] = useState('')
+
+  const handleSearchUser=(e:React.ChangeEvent<HTMLInputElement>)=>{
+    setSearchUser(e.target.value)
+    }
+const fetchusers = async()=>{
+  try {
+    const response = await fetch('/users.json')
+    if (!response.ok) {
+      throw new Error;
+      
+    }
+    
+  } catch (error) {
+    console.error('error')
+  }
+}
+
   return (
     <div className="user-card">
         <div className="search-section">
